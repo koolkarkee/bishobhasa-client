229f78b43092f6c5342566b4043626b3bcdeb72a
Bibek Karki <koolkarkee@gmail.com>
2021-07-14 08:48:42 +0545

    test

---
+++ build/precache-manifest.af3a3f5f4a7f14bdf8503fb368d6db9e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee616712e276c7012ed74d5cd9bffc0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "691f7bb70f694751dc4b",
+    "url": "/static/css/2.f1d36a8f.chunk.css"
+  },
+  {
+    "revision": "1d879d0bf96a0409717d",
+    "url": "/static/css/main.fa00fd84.chunk.css"
+  },
+  {
+    "revision": "691f7bb70f694751dc4b",
+    "url": "/static/js/2.8d5d846d.chunk.js"
+  },
+  {
+    "revision": "705c5ef662b371952dc67842cd193465",
+    "url": "/static/js/2.8d5d846d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d164f9155dc10767abec",
+    "url": "/static/js/3.73dbdf30.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.73dbdf30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1898439a468b3ea97475",
+    "url": "/static/js/4.a825b19e.chunk.js"
+  },
+  {
+    "revision": "809abad1ab3333af372fdc0cd7f2829d",
+    "url": "/static/js/4.a825b19e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9389d01671ff6273b89d",
+    "url": "/static/js/5.55bbcfb1.chunk.js"
+  },
+  {
+    "revision": "59032cdf40f811ad3c6d189d08d01426",
+    "url": "/static/js/5.55bbcfb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d879d0bf96a0409717d",
+    "url": "/static/js/main.b5507d54.chunk.js"
+  },
+  {
+    "revision": "3fed560887de4b0e1125",
+    "url": "/static/js/runtime-main.90ddbc89.js"
+  }
+]);
