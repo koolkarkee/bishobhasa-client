ddbc903bd80676c6f604316c2803236424bd0e1b
koolkarkee <koolkarkee@gmail.com>
2021-11-23 09:45:37 +0545

    updating map on the contacts page

--- src/components/site/contact/contact.component.js
+++ src/components/site/contact/contact.component.js
@@ -118,11 +118,13 @@ const ContactComponent = function(props) {
 
             </div>
           </section> 
-          <section class="hero notification is-light mb-0 px-5">
+          <section class="hero notification is-light mb-0 px-5 mx-5">
               <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d220.75956785730145!2d85.3220703!3d27.7125593!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3bffdf1430cf013d!2sIDP+Education+Nepal+-+International+Education+Specialists!5e0!3m2!1sen!2sin!4v1546500493448" 
-                height="450" title="bishobhasamap"
-                style={{border:1}}></iframe>
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3531.8659119985573!2d85.34401771412003!3d27.721426031482064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1965a381634d%3A0xffd4205d0a82a2b!2sCharumati%20Bhawan%2C%20Gopikrishna%2C%20Ring%20Road%2C%20Kathmandu%2044600!5e0!3m2!1sen!2snp!4v1637638871639!5m2!1sen!2snp" 
+                height="400" loading="lazy" 
+                title="bishobhasamap"
+                style={{border:1}}
+                ></iframe>  
           </section>
         </>  
     )
